590249f384e120cd3727eb8b5390df108878a284
FarToday <114233356+FarToday@users.noreply.github.com>
2022-11-05 20:15:47 +0800

    Update jam.js

--- jam.js
+++ jam.js
@@ -3,19 +3,23 @@ refresh();
 function refresh(){
 
 var a = new Date();
+var format = "AM";
 
 var jam = a.getHours();
 var menit = a.getMinutes();
 // var detik = a.getSeconds();
   
   if (jam>=13&&jam<24){
-    jam -= 12;}
+    jam -= 12;
+    format = "PM";
+    }
   
   if (menit<10){
     jam = "0"+jam;}
 
 document.getElementById("jam").innerHTML = jam;
 document.getElementById("menit").innerHTML = menit;
+document.getElementById("ampm").innerHTML = format;
 // document.getElementById("detik").innerHTML = detik;
 setTimeout(refresh, 1000);
 }
