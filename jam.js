037452fca2ce0b4fca7596f747b30771edd15bdd
FarToday <114233356+FarToday@users.noreply.github.com>
2022-11-05 20:13:00 +0800

    Update jam.js

--- jam.js
+++ jam.js
@@ -7,6 +7,12 @@ var a = new Date();
 var jam = a.getHours();
 var menit = a.getMinutes();
 // var detik = a.getSeconds();
+  
+  if (jam>=13&&jam<24){
+    jam -= 12;}
+  
+  if (menit<10){
+    jam = "0"+jam;}
 
 document.getElementById("jam").innerHTML = jam;
 document.getElementById("menit").innerHTML = menit;
